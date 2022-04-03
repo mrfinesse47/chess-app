b90bfb67da4fb5477d71588264430677353086e1
Simon Davis <simon.davis@secureideas.com>
2022-04-02 20:48:51 -0400

    Added label should show test for TextField

--- libs/ui/src/lib/text-field/text-field.spec.tsx
+++ libs/ui/src/lib/text-field/text-field.spec.tsx
@@ -4,7 +4,11 @@ import TextField from './text-field';
 
 describe('TextField', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<TextField />);
+    const { baseElement } = render(<TextField id="test" label="Test" />);
     expect(baseElement).toBeTruthy();
+  });
+  it('label should display text', () => {
+    const { getByText } = render(<TextField id="test" label="Test" />);
+    expect(getByText('Test')).toBeTruthy();
   });
 });
