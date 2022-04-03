9982ee85c6fae1a87e71c77ead4a4fa92731224d
Simon Davis <simon.davis@secureideas.com>
2022-04-03 14:42:23 -0400

    working Login and Register link buttons

--- apps/chess/pages/index.tsx
+++ apps/chess/pages/index.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { User } from "@chess/utils";
-import Link from "next/link";
+import { LinkButton } from "@chess/ui";
 const StyledPage = styled.div`
   display: flex;
   justify-content: center;
@@ -8,10 +8,6 @@ const StyledPage = styled.div`
   width: 100vw;
   height: 100vh;
   font-size: 1.5rem;
-`;
-const LinkButton = styled(Link)`
-  padding: 1em;
-  background: orange;
 `;
 
 const Navbar = styled.nav``;
@@ -57,8 +53,12 @@ export function Index() {
         <Title>Chess</Title>
         <SubTitle>{`K&S Edition`}</SubTitle>
         <CTAButtonsGroup>
-          <LinkButton href="/login">Login</LinkButton>
-          <LinkButton href="/register">Register</LinkButton>
+          <LinkButton variant={"primary"} href="/login">
+            Login
+          </LinkButton>
+          <LinkButton variant={"secondary"} href="/register">
+            Register
+          </LinkButton>
         </CTAButtonsGroup>
         <StatsSection>
           <Leaderboard>
