709e7328484aed6c05267aec5fd1c9c88c60376a
Kevin Mason <krmason2020@gmail.com>
2022-04-03 13:37:21 -0700

    updates to user table

--- apps/chess-api/src/db/schema/00_users.sql
+++ apps/chess-api/src/db/schema/00_users.sql
@@ -2,14 +2,10 @@ DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
+  user_name VARCHAR(255) NOT NULL UNIQUE,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  address VARCHAR NOT NULL,
-  neighborhood VARCHAR NOT NULL,
-  borrower BOOLEAN NOT NULL,
-  lender BOOLEAN NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  cash_balance_cents INTEGER NOT NULL,
-  phone VARCHAR(255) NOT NULL,
+  rating INTEGER NOT NULL,
   password VARCHAR(255) NOT NULL
 );
--- apps/chess-api/src/db/seeds/00_users_seeds.sql
+++ apps/chess-api/src/db/seeds/00_users_seeds.sql
@@ -1,9 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (first_name, last_name, address, neighborhood, borrower, lender, email, cash_balance_cents, phone, password)
+INSERT INTO users (user_name, first_name, last_name, email, rating, password)
 VALUES
-  ('Nicole', 'Dunn', '1400 BROADWAY ST', 'Kitsilano', true, true, 'test@test.com', 1000997, '1234567890', '123'),
-  ('Cole', 'Monroe', '1760 Pacific ST', 'West End', true, false, 'test2@test.com', 100000, '6725554444', '123'),
-  ('Luke', 'Gray', '1460 main', 'Mount Pleasant', true, true, 'test3@test.com', 100000, '6714445555', '123'),
-  ('Orlando', 'Ross', '1700 Davie St', 'West End', true, true, 'mauris@protonmail.edu', 100000, '6719998888', 'password'),
-  ('Brady', 'Mcmahon', '760 8th Ave', 'Kitsilano', true, false, 'elit.etiam.laoreet@yahoo.org', 100000, '6728889999', 'password'),
-  ('Danielle', 'Harvey', '700 Water St', 'Gastown', true, true, 'ligula.donec@google.ca', 100000, '6716571234', 'password');
+  ('tester', 'Nicole', 'Dunn', 'test@test.com',1600,'123'),
+  ('mrfinesse47', 'Kevin', 'Mason', 'test2@test.com',1600 ,'123');
