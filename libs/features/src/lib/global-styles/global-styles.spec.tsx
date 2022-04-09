aaf092cfdba0b0d099abc1777d2b66ed2b8c70be
Simon Davis <simon.davis@secureideas.com>
2022-04-08 22:21:09 -0400

    Fixed GlobalStyles import in test

--- libs/features/src/lib/global-styles/global-styles.spec.tsx
+++ libs/features/src/lib/global-styles/global-styles.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 
-import GlobalStyles from "./global-styles";
+import { GlobalStyles } from "./global-styles";
 
 describe("GlobalStyles", () => {
   it("should render successfully", () => {
