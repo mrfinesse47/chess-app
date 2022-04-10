5cd491c7299f5d385e2d022e3989ee978c872223
Simon Davis <simon.davis@secureideas.com>
2022-04-10 17:13:22 -0400

    Fixed Test for root Index

--- apps/chess/specs/index.spec.tsx
+++ apps/chess/specs/index.spec.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import Index from "../pages/index";
+import { UserSessionProvider } from "@chess/features";
 
 describe("Index", () => {
   it("should render successfully", () => {
-    const { baseElement } = render(<Index />);
+    const { baseElement } = render(
+      <UserSessionProvider>
+        <Index />
+      </UserSessionProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
