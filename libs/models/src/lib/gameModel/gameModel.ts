73951c21507a6a3a66b27b79d0037afc63fa63f5
Simon Davis <simon.davis@secureideas.com>
2022-04-30 14:21:41 -0400

    added events to game and player model

--- libs/models/src/lib/gameModel/gameModel.ts
+++ libs/models/src/lib/gameModel/gameModel.ts
@@ -1,8 +1,21 @@
 import { createModel } from "xstate/lib/model";
 
 export const gameModel = createModel(
-  {},
   {
-    events: {},
+    board: [],
+    player1: {},
+    player2: {},
+    activePlayer: {},
+    moves: 0,
+  },
+  {
+    events: {
+      INITIALIZE_GAME: () => ({}),
+      CHOOSE_PLAYER: () => ({}),
+      DRAW: () => ({}),
+      START_GAME: () => ({}),
+      PAUSE_GAME: () => ({}),
+      END_GAME: () => ({}),
+    },
   }
 );
--- libs/models/src/lib/playerModel/playerModel.ts
+++ libs/models/src/lib/playerModel/playerModel.ts
@@ -1,8 +1,22 @@
 import { createModel } from "xstate/lib/model";
 
 export const playerModel = createModel(
-  {},
   {
-    events: {},
+    username: "",
+    isActive: false,
+    assignedColor: "",
+    timeLeft: 0,
+  },
+  {
+    events: {
+      SELECT_PIECE: () => ({}),
+      MOVE_PIECE: () => ({}),
+      CANCEL_MOVE: () => ({}),
+      UNDO: () => ({}),
+      REDO: () => ({}),
+      CALL_FOR_DRAW: () => ({}),
+      LEAVE_GAME: () => ({}),
+      SURRENDER: () => ({}),
+    },
   }
 );
